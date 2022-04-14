3b466870e000ad83d9cf8c19aeec31aa8d9be3f8
Abhijit Jathar <abbyjeet@gmail.com>
2022-04-14 14:40:18 +1000

    Refactoring and different size widgets

--- ipl.js
+++ ipl.js
@@ -10,21 +10,42 @@ based on:
 The Golf Club Wdiget by @supermamon
 */
 
+// arguments
+const WARG = args.widgetParameter
+const widgetFamily = config.widgetFamily ? config.widgetFamily : 'large'
+
 // preferences
-const FONT_SIZE = 12
-const EXCLUDE_HC = true
-const HEADER_COLOR = Color.red()
-const ROW_COLOR = Color.blue()
-const BACKGROUND_COLOR = Color.black()
-const url = 'https://ipl-stats-sports-mechanic.s3.ap-south-1.amazonaws.com/ipl/feeds/stats/60-groupstandings.js'
+const COLOR_BLUE1 = "#19398a"
+const COLOR_BLUE2 = "#18184a"
+const BGIMAGE = "https://raw.githubusercontent.com/abbyjeet/scriptable-widgets/main/ipl.jpg"
+const FONT7 = Font.regularSystemFont(7) 
+const FONT12 = Font.regularSystemFont(12) 
+const FONT14 = Font.boldSystemFont(14)
+const COLWIDTHS = [25,172,25,40,40]
+const HEIGHT = 29
+const EXCLUDE_HC = false //headercells
+const HEADERS = ["", "Team","Pts","NRR","Form"]
+const KEYS = ["TeamLogo", "TeamName","Points","NetRunRate","Performance"]
 
-const bgimage = "https://raw.githubusercontent.com/abbyjeet/scriptable-widgets/main/ipl.jpg"
 
-const sc = new Color("#19398a",0.95)
-const ec = new Color("#18184a",0.95)
+// URLs
+const pointstable = 'https://ipl-stats-sports-mechanic.s3.ap-south-1.amazonaws.com/ipl/feeds/stats/60-groupstandings.js'
+const schedule = 'https://ipl-stats-sports-mechanic.s3.ap-south-1.amazonaws.com/ipl/feeds/60-matchschedule.js'
+
+// Background color
+const sc = new Color(COLOR_BLUE1,0.95)
+const ec = new Color(COLOR_BLUE2,0.95)
 const gradC = new LinearGradient()
     gradC.colors = [sc, ec]
     gradC.locations = [0,1]
+
+// widget
+const widget = new ListWidget()
+
+// apply background image and background color
+let imgBg = await new Request(BGIMAGE).loadImage()
+widget.backgroundImage = imgBg
+widget.backgroundGradient = gradC
 
 
 //callback function// 
@@ -33,124 +54,126 @@ function ongroupstandings(x){
 }
 
 // call api, execute callback and get data
-async function loadItems(){
-    let req = new Request(url)
+async function loadPointsTable(){
+    let req = new Request(pointstable)
     let json = await req.loadString()
     let data = eval("(function(){ return " + json + "}())")
     return data    
 }
 
+function pointsTableWidget(){
+    const pointsTable = await loadPointsTable()
 
+    const rows = pointsTable.slice(0, widgetFamily=='large'?10:4)
 
-const font = Font.regularSystemFont(FONT_SIZE) 
-
-// adjust only if preferred
-// const colWidths = [45,120,45,45,45,45]
-
-const colWidths = [25,172,25,40,40]
-
-const data = await loadItems()
-
-const headers = ["", "Team","Pts","NRR","Form"]
-
-const keys = ["TeamLogo", "TeamName","Points","NetRunRate","Performance"]
-
-const widgetFamily = config.widgetFamily ? config.widgetFamily : 'large'
-
-const HEIGHT = 29 // 
-// widgetFamily == "large" ? 29 : 24
-
-const widget = new ListWidget()// 
-// widget.backgroundColor = BACKGROUND_COLOR
-
-let imgBg = await new Request(bgimage).loadImage()
-widget.backgroundImage = imgBg
-widget.backgroundGradient = gradC
-// widget.setPadding(0,0,0,0)
-
-// add the headers
-const headerStack = widget.addStack()
-headerStack.layoutHorizontally()
-// headerStack.setPadding(0,0,0,0)
-
-let idx = -1
-for (header of headers) {
-  idx++
-  if (EXCLUDE_HC && idx==5) continue;
-  const headerText = header
-  const headerCell = headerStack.addStack()
-  headerCell.layoutHorizontally()
-//   headerCell.backgroundColor = HEADER_COLOR
-  headerCell.size = new Size(colWidths[idx],HEIGHT)
-  
-//   headerCell.addSpacer(2)
-  const textElement = headerCell.addText(headerText)
-  textElement.font = Font.boldSystemFont(14)
-  
-  headerStack.addSpacer(1) // between cells
-  
-  headerCell.centerAlignContent()
-  
-  if(idx==1) headerCell.addSpacer()
-}
-
-widget.addSpacer(1)
-// add the data
-
-// limit the number of rows based on widget size
-
-const rows = data.slice(0, widgetFamily=='large'?10:4)
-
-for (const row of rows) {
-  const rowStack = widget.addStack()
-  rowStack.layoutHorizontally()
-  let idx = -1
-  for (const cell of keys) {
-    idx++
-    if (EXCLUDE_HC && idx==5) continue;
-    const cellText = row[cell]
     
-    if(idx==0){ 
-       // logo
-      const dataCell = rowStack.addStack() 
+    // add the headers
+    const headerStack = widget.addStack()
+    headerStack.layoutHorizontally()
+    // headerStack.setPadding(0,0,0,0)
+
+    let idx = -1
+    for (header of HEADERS) {
+      idx++
+      if (EXCLUDE_HC && idx==5) continue;
+      const headerText = header
+      const headerCell = headerStack.addStack()
+      headerCell.layoutHorizontally()
+        //   headerCell.backgroundColor = HEADER_COLOR
+      headerCell.size = new Size(COLWIDTHS[idx],HEIGHT)
+
+        //   headerCell.addSpacer(2)
+      const textElement = headerCell.addText(headerText)
+      textElement.font = FONT14
+
+      headerStack.addSpacer(1) // between cells
+
+      headerCell.centerAlignContent()
+
+      if(idx==1) headerCell.addSpacer()
+    }
+
+    widget.addSpacer(1)
+
+
+    for (const row of rows) {
+      const rowStack = widget.addStack()
+      rowStack.layoutHorizontally()
+      let idx = -1
+      for (const cell of KEYS) {
+        idx++
+        if (EXCLUDE_HC && idx==5) continue;
+        const cellText = row[cell]
+
+        if(idx==0){ 
+           // logo
+          const dataCell = rowStack.addStack() 
+            dataCell.layoutHorizontally()
+        //     dataCell.backgroundColor = Color.blue()
+        dataCell.size = new Size(COLWIDTHS[idx],HEIGHT)
+            let imgObj = await new Request(cellText).loadImage()
+            let img = dataCell.addImage(imgObj)
+        } else {
+
+        const dataCell = rowStack.addStack()
         dataCell.layoutHorizontally()
-//     dataCell.backgroundColor = Color.blue()
-    dataCell.size = new Size(colWidths[idx],HEIGHT)
-        let imgObj = await new Request(cellText).loadImage()
-        let img = dataCell.addImage(imgObj)
-    } else {
-    
-    const dataCell = rowStack.addStack()
-    dataCell.layoutHorizontally()
-//     dataCell.backgroundColor = Color.blue()
-    dataCell.size = new Size(colWidths[idx],HEIGHT)
-    
-    dataCell.addSpacer(1)
-    
-    let textElement = ""
-    if(idx==4){
-//       cellText = "W,W,L,W,L"
-      let balls = cellText.replaceAll(",", "").replaceAll("W", "🟢").replaceAll("L", "🔴")
-      balls = [...balls].reverse().join("").substring(0,6)
-// 
-// dataCell.addSpacer(1)
-      textElement=dataCell.addText(balls)
-      textElement.font = Font.regularSystemFont(7)
-}else{
-  textElement=dataCell.addText(cellText)
-  textElement.font = font
-}
-    
-    dataCell.centerAlignContent()
-    
-    if(idx==1) dataCell.addSpacer()
-  }
-    
-    rowStack.addSpacer(1)  // between cells
-  } 
-  widget.addSpacer(1) // between rows
+        //     dataCell.backgroundColor = Color.blue()
+        dataCell.size = new Size(COLWIDTHS[idx],HEIGHT)
+
+        dataCell.addSpacer(1)
+
+        let textElement = ""
+        if(idx==4){
+            //       cellText = "W,W,L,W,L"
+          let balls = cellText.replaceAll(",", "").replaceAll("W", "🟢").replaceAll("L", "🔴")
+          balls = [...balls].reverse().join("").substring(0,6)
+            // 
+            // dataCell.addSpacer(1)
+          textElement=dataCell.addText(balls)
+          textElement.font = FONT7
+    }else{
+      textElement=dataCell.addText(cellText)
+      textElement.font = FONT12
+    }
+
+        dataCell.centerAlignContent()
+
+        if(idx==1) dataCell.addSpacer()
+      }
+
+        rowStack.addSpacer(1)  // between cells
+      } 
+      widget.addSpacer(1) // between rows
+    }   
 }
 
+function favouriteTeamWidget(){
+    // add the text
+    const textEl = widget.addText("Favourite")
+}
+
+function upcomingMatchWidget(){
+    // add the text
+    const textEl = widget.addText("Upcoming")
+}
+
+function lastMatchWidget(){
+    // add the text
+    const textEl = widget.addText("Last match")
+} 
+
+// present widget - in app or in widget
+if (widgetFamily === "small") {
+    favouriteTeamWidget()
+} else if (widgetFamily === "medium" && warg === "pt") {
+    pointsTableWidget()
+} else if (widgetFamily === "medium" && warg === "upc") {
+    upcomingMatchWidget()
+} else if (widgetFamily === "medium" && warg === "last") {
+    lastMatchWidget()
+} else {
+    pointsTableWidget()
+}
 
 if (config.runsInApp) await widget[`present${widgetFamily.split('').map( (c,i)=>(i==0?c.toUpperCase():c)).join('')}`]()
 Script.setWidget(widget)
